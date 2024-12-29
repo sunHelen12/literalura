0c518e79a545db86a26a4b9bfb0422a992c2e833
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-29 19:24:47 -0300

    Adição de Entidades Livro e Autor Adição de records para tratar do Json Adição de Conversor de dados e Interface de Conversor de dados Criação da classe principal com interface de usuário Classe ConsumoAPI para conexão de API

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,23 @@
 package br.com.alura.literalura;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.literalura.principal.Principal;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
---
+++ src/main/java/br/com/alura/literalura/model/Livro.java
@@ -0,0 +1,5 @@
+package br.com.alura.literalura.model;
+
+public class Livro {
+
+}
