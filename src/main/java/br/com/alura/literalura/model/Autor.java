37173df4d6aac71418c06028a50c0fbc69d4c29d
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-30 13:52:29 -0300

    Alteração nas classes Autor, no enum Idioma,

--- src/main/java/br/com/alura/literalura/model/Autor.java
+++ src/main/java/br/com/alura/literalura/model/Autor.java
@@ -1,10 +1,77 @@
 package br.com.alura.literalura.model;
 
+import java.util.List;
+
+import br.com.alura.literalura.model.records.DadosAutor;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "autores")
 public class Autor{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String nome;
     private Integer dataNascimento;
     private Integer dataFalecimento;
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Livro> livros;
 
+    public Autor(){}
+
+    public Autor(DadosAutor autor){
+        this.nome = autor.nome();
+        this.dataNascimento = autor.dataNascimento();
+        this.dataFalecimento = autor.dataFalecimento();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Integer dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public Integer getDataFalecimento() {
+        return dataFalecimento;
+    }
+    public void setDataFalecimento(Integer dataFalecimento) {
+        this.dataFalecimento = dataFalecimento;
+    }
+    public List<Livro> getLivros() {
+        return livros;
+    }
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor - ID:" + id 
+                + ", Nome: " + nome 
+                + ", Data de Nascimento: " + dataNascimento 
+                + ", Data de Falecimento: " + dataFalecimento 
+                + ", Livros: " + livros;
+    }
     
 }
--- src/main/java/br/com/alura/literalura/model/Livro.java
+++ src/main/java/br/com/alura/literalura/model/Livro.java
@@ -8,7 +8,7 @@ public class Livro {
     private Long id;
     private String titulo;
     private List<Autor> autores;
-    private Idiomas idiomas;
+    private Idioma idiomas;
     private Double numeroDeDownloads;
 
 }
